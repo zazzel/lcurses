70f25f7e181b4d4cf02f5024414ad1d4eb665a1d
JJ Andresson <jjandresson@gmail.com>
2016-01-10 20:12:51 +0000

    maint: manage sanity checks.
    * build-aux/sanity-cfg.mk: New file.
    
    Signed-off-by: JJ Andresson <jjandresson@gmail.com>

---
+++ build-aux/sanity-cfg.mk
@@ -0,0 +1,5 @@
+exclude_file_name_regexp--sc_require_config_h = ^ext/include/(compat-5\.2|strlcpy)\.c|ext/curses/curses\.c$$
+exclude_file_name_regexp--sc_require_config_h_first = ^ext/include/(compat-5\.2|strlcpy)\.c|ext/curses/curses\.c$$
+exclude_file_name_regexp--sc_useless_cpp_parens = ^ext/include/compat-5\.2\.[ch]$$
+
+EXTRA_DIST += build-aux/sanity-cfg.mk
